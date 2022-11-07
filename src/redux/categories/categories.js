616965fed5599c903324a604ac6f2cc254e16a11
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-07 12:09:50 -0500

    Setup actions and reducers

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,18 @@
+const CHECK_STATUS = 'book-store/categories/CHECK_STATUS';
+const FILTER_BOOKS = 'book-store/categories/FILTER_BOOKS';
+
+const STATUS = 'Under Construction';
+
+const categoriesReducer = (state = [], action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return STATUS;
+    
+    default:
+      return state;
+  }
+};
+
+export const checkStatus = () => ({ type: CHECK_STATUS });
+
+export default categoriesReducer;
