ac8345ced836e26cd3886a1e5bb1a09cc7928963
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-07 12:10:06 -0500

    Fix linter error

---
+++ src/redux/books/books.js
@@ -0,0 +1,40 @@
+const ADD_BOOK = 'book-store/books/ADD_BOOK';
+const REMOVE_BOOK = 'book-store/books/REMOVE_BOOK';
+const initialState = [
+  {
+    id: '1',
+    title: 'title 1',
+    author: 'author 1',
+  },
+  {
+    id: '2',
+    title: 'title 2',
+    author: 'author 2',
+  },
+  {
+    id: '3',
+    title: 'title 3',
+    author: 'author 3',
+  },
+];
+
+const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((item) => item.id !== action.payload);
+    default: return state;
+  }
+};
+const addBook = (book) => ({
+  type: ADD_BOOK,
+  payload: book,
+});
+const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  payload: id,
+});
+
+export default bookReducer;
+export { addBook, removeBook };
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,5 +1,4 @@
 const CHECK_STATUS = 'book-store/categories/CHECK_STATUS';
-const FILTER_BOOKS = 'book-store/categories/FILTER_BOOKS';
 
 const STATUS = 'Under Construction';
 
@@ -7,7 +6,6 @@ const categoriesReducer = (state = [], action) => {
   switch (action.type) {
     case CHECK_STATUS:
       return STATUS;
-    
     default:
       return state;
   }
