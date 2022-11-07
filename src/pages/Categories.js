afe49c8557c05f75a36521acc9ef71e3ea7cd972
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-07 15:54:28 -0500

    Add Caategories screen

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <h1>Categories</h1>
-    <button type="button">Check status</button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.category);
+  return (
+    <>
+      <p>{status}</p>
+      <button type="button" onClick={() => dispatch(checkStatus())}>
+        Check Status
+      </button>
+    </>
+  );
+};
 
 export default Categories;
