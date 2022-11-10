5ea267bd720a2948324692c91c11b71d3b101fe4
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-10 10:28:53 -0500

    Retrieve Categories

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux/es/exports';
-import { checkStatus } from '../redux/categories/categories';
+import girl from '../assets/girl.png';
+import { checkStatus } from '../redux/reducers/categories';
 
 const Categories = () => {
   const dispatch = useDispatch();
-  const status = useSelector((state) => state.category);
+  const status = useSelector((state) => state.rootReducer.categories);
   return (
-    <>
-      <p>{status}</p>
-      <button type="button" onClick={() => dispatch(checkStatus())}>
+    <div className="mt-[200px] flex flex-col items-center justify-center">
+      <p className="font-bold text-4xl text-gray-600">{status}</p>
+      <img src={girl} className="my-10 rounded-full outer inner p-4" alt="construction" />
+      <button className="bg-[#54B4FF] px-2 py-1 text-white rounded" type="button" onClick={() => dispatch(checkStatus())}>
         Check Status
       </button>
-    </>
+    </div>
   );
 };
 
